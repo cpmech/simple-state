8ae9ba8418adc8cc902663b5f339206227b670ee
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-02-13 19:12:53 +1000

    Remove husky and lintstaged

--- scripts/copy_dist_to_app.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yarn build
-rsync -av dist/* ../epop-v2/epop/node_modules/@cpmech/simple-state/dist/
-rsync -av dist/* ../epop-v2/epop-app/node_modules/@cpmech/simple-state/dist/
-rsync -av dist/* ../epop-v2/epop-manager/node_modules/@cpmech/simple-state/dist/
